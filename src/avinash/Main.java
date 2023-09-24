9e75c5cfe1faa5716fc23de1e14df4fe3f524702
Avinash Suthar <avinashsuthar19hmh@gmail.com>
2023-09-24 14:35:31 +0530

    mainin avinash is modified

--- src/avinash/Main.java
+++ src/avinash/Main.java
@@ -3,6 +3,6 @@ package avinash;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello World");
-
+        System.out.println("hello i am avinash and learning github");
     }
 }
