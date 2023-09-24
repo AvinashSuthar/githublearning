569343c77045525a231932d378081a68855428ec
Avinash Suthar <avinashsuthar19hmh@gmail.com>
2023-09-24 22:02:18 +0530

    updated

---
+++ src/avinash/FunctionOverloading.java
@@ -0,0 +1,17 @@
+package avinash;
+
+public class FunctionOverloading {
+    public static void main(String[] args) {
+        fun(65);
+        fun("Avinash");
+    }
+    static  void fun(int a){
+        // ambiguity :
+
+    }
+    static  void fun(String a){
+
+    }
+
+
+}
